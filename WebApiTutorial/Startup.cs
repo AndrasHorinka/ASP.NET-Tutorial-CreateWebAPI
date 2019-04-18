e3e1da1da314018532daabb5b2810ca5ec31b895
AndrasHorinka <horinka.andras@gmail.com>
2019-04-18 09:53:07 +0200

    home

--- WebApiTutorial/Startup.cs
+++ WebApiTutorial/Startup.cs
@@ -1,30 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using WebApiTutorial.Models;
 
 namespace WebApiTutorial
 {
     public class Startup
     {
+        // Constructor
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        // Property for Configuration
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -37,6 +35,8 @@ namespace WebApiTutorial
             }
             else
             {
+                // The default HSTS value is 30 days. You may want to change this for 
+                // production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
